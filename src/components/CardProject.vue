6b55ea982b694bdd6a6c38247536b95e63dce044
GaranceVermorel <74402918+guacax@users.noreply.github.com>
2024-07-08 17:21:08 +0200

    Update CardProject.vue

--- src/components/CardProject.vue
+++ src/components/CardProject.vue
@@ -1,9 +1,19 @@
 <script setup>
 
+const props = defineProps({
+    img: String,
+    name: String,
+    desc: Text
+})
+
 </script>
 
 <template>
 
-   
+    <div class="cardProject">
+        <img :src="img" :alt="name">
+        <h2>{{ name }}</h2>
+        <p>{{ desc }}</p>
+    </div>
 
 </template>
