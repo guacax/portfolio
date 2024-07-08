206f503eb5079d9f9c29a1dab124072af50eb0f7
GaranceVermorel <74402918+guacax@users.noreply.github.com>
2024-07-08 14:57:07 +0200

    links

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,18 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
+
 </script>
 
 <template>
   <main>
-    <h1>Salut, I'm Gua or Garance
-      <br>
-      And i'm a web developper
-    </h1>
-    <h2>Welcome to my portfolio</h2>
+    <div class="opening">
+      <h1>Hey, I'm Garance
+        <br>
+        And i'm a web developer
+      </h1>
+      
+    </div>
 
-    <p>If you want to know more <a>about me</a>, my <a>projects</a>, about my <a>work experience</a> or to know how to <a>contact</a> me, just click on the browser up there :)</p>
+    <p class="links_home">If you want to know more <RouterLink to='/about'>about me</RouterLink>, my <RouterLink to='/projects'>projects</RouterLink>, about my <RouterLink to='/experience'>work experience</RouterLink> or to know how to <RouterLink to='/contact'>contact</RouterLink> me, just click on the menu up there :) (or on the links in this paraghape)</p>
   </main>
 </template>
