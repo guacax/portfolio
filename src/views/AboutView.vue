d3fc39cccea85a0984e502d97810ae58605df296
GaranceVermorel <74402918+guacax@users.noreply.github.com>
2024-07-09 17:30:35 +0200

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,10 @@
 <template>
   <main>
     <h1>About me</h1>
-    <p>One of my favorite part when i re-do my portfolio is the about me page, mostly because i don't have to go all my way out on the front (whichc i actually really like to do);, but also because i'm kinda megalo.</p>
     <p>I was born in Paris not so long ago (21/10/2003), same day as my dad (tmi), and i was raised in Saint-Rémy-les-Cheuvreses near Veersailles until 2011, when my familly move to Vernon in Normandy</p>
-    <p>I'm the youngest in my familly of three, so obvsouisly all lot of what i enjoy is a mix of they like, for example: video games, cartoons, every genre music, art. I also like some things thaht they don't, like biking and going on hikes, oh and also coding</p>
-    <p>Btw, i started to code when i was 15 (late bloomer for some), at first i enjoyed it but i didn't think that i would like to pursue it for my carrer, since i wanted to be an aeropsatial engineer. When i realised i didn't have to competence and also i wanted to make space just a passion and not more</p>
-    <p>I'ts in 2021 that i got myself back to code, and i reallllly like it, i mostly do kinda full stack, but i have the basics in app/software and i want to learn how to make simple video games</p>
-    <p>What i do on my free time : stay at home to play video games and sleep, go to the musuems, go to the cinema and got to visit my familly and friends outside or inside Paris</p>
+    <p>I'm the youngest in my familly of three, so obvsouisly all lot of what i enjoy is a mix of what they like, for example: video games, cartoons, every genre music, art. I also like some things that they don't, like biking and going on hikes, oh and also coding</p>
+    <p>Btw, i started to code when i was 15 (late bloomer for some, ik ik), at first i enjoyed it but i didn't think that i would like to pursue it for my carrer, since i wanted to be an aeropsatial engineer. But then i realised i didn't have to competence and also i wanted to make space just a passion and not more</p>
+    <p>It's in 2021 that i got myself back to code, and i reallllly liked it, i mostly do kinda full stack, but i have the basics in app/software and i want to learn how to make simple video games</p>
+    <p>What i do on my free time : stay at home to play video games and sleep, go to the museums, to the cinema and visit my familly and friends outside or inside of Paris</p>
   </main>
 </template>
