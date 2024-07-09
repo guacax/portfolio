d8d851dc93ce37a8c5e2afb73d650ad0fe070393
GaranceVermorel <74402918+guacax@users.noreply.github.com>
2024-07-09 17:30:54 +0200

    affichage des projets via les fichiers json

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,27 +1,32 @@
 <script setup>
 
-import project from './assets/projectPerso.json'
+import cardProject from '@/components/CardProject.vue'
+import schoolProjects from '../assets/schoolProjects.json'
+import persoProjects from '../assets/persoProjects.json'
 
-export default {
-  data() {
-    return {
-        projects: project
-    };
-  },
-};
+
+
 </script>
 
 <template>
-    <main>
-        <h1>School Projects</h1>
-        <div>
-    
-        </div>
-    
-        <h1>Personnal Projects</h1>
-        <div>
-    
-        </div>
+	<main>
 
-    </main>
+		<h1>School Projects</h1>
+
+		<div class="projects">
+			<div v-for="schoolProject in schoolProjects" :key="schoolProject.id" class="schoolProject">
+				<cardProject :name="schoolProject.name" :img="schoolProject.img" :time="schoolProject.time"
+					:desc="schoolProject.desc" :language="schoolProject.language"></cardProject>
+			</div>
+
+		</div>
+		<h1>Personnal Projects</h1>
+		<div class="projects">
+			<div v-for="persoProject in persoProjects" :key="persoProject.id">
+				<cardProject :name="persoProject.name" :img="persoProject.img" :time="persoProject.time"
+					:desc="persoProject.desc" :language="persoProject.language"></cardProject>
+			</div>
+		</div>
+
+	</main>
 </template>
