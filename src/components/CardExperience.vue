5e27e616e1aff5f747ad994558f2be031966b49d
GaranceVermorel <74402918+guacax@users.noreply.github.com>
2024-07-10 11:09:24 +0200

    Update CardExperience.vue

--- src/components/CardExperience.vue
+++ src/components/CardExperience.vue
@@ -1,10 +1,19 @@
 <script setup>
 
-
+const props = defineProps({
+    title: String,
+    entreprise: String,
+    date: String,
+    desc: Text
+})
 
 </script>
 
 <template>
- 
+
+    <h2>{{ title }}</h2>
+    <h3>{{ entreprise }}</h3>
+    <em>{{ date }}</em>
+    <p>{{ desc }}</p>
  
 </template>
