dd9dae6b0c92d9919aa16b60ee546e7e59a2f514
GaranceVermorel <74402918+guacax@users.noreply.github.com>
2024-07-10 11:09:17 +0200

    Update ContactView.vue

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -5,5 +5,12 @@
     <main>
 
         <h1>Wanna talk ?</h1>
+        <p>Email : xxx@gmail.com</p>
+        <p>Phone : XX.XX.XX.XX.XX</p>
+        <p>Socials : 
+            <br>if your want to see that i'm very unhinged and a big leftist : twitter
+            <br>if your prefer pictures of chairs + still leftist story post : insta
+            <br>if you just want to connect and see nothing from me : linkedin
+        </p>
     </main>
 </template>
