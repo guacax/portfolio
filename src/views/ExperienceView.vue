cc183283f7711d2c998559e9aaf72bccaf62d55f
GaranceVermorel <74402918+guacax@users.noreply.github.com>
2024-07-10 11:09:15 +0200

    Update ExperienceView.vue

--- src/views/ExperienceView.vue
+++ src/views/ExperienceView.vue
@@ -1,10 +1,26 @@
 <script setup>
 
+import CardExperience from '@/components/CardExperience.vue';
+import workExps from '../assets/workExp.json';
+import schoolExps from '../assets/schoolExp';
+
 </script>
 
 <template>
     <main>
         <h1>Work experiences</h1>
-
+        <div class="exps">
+            <div v-for="workExp in workExps" :key="workExp.id">
+                <CardExperience :title="workExp.title" :entreprise="workExp.entreprise" :date="workExp.date"
+                    :desc="workExp.desc"></CardExperience>
+            </div>
+        </div>
+        <h1>School experiences</h1>
+        <div class="exps">
+            <div v-for="schoolExp in schoolExps" :key="schoolExp.id">
+                <CardExperience :title="schoolExp.title" :entreprise="schoolExp.entreprise" :date="schoolExp.date"
+                    :desc="schoolExp.desc"></CardExperience>
+            </div>
+        </div>
     </main>
 </template>
